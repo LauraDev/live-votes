3b8db4bfde3ad41b415daf82df0d8d714b1f0c70
ltraore <ltraore@sqli.com>
2018-10-18 16:33:40 +0200

    server v2

--- voting-server/index.js
+++ voting-server/index.js
@@ -2,10 +2,14 @@ import makeStore from './src/store';
 import startServer from './src/server';
 
 export const store = makeStore();
+
 startServer(store);
 
 store.dispatch({
     type: 'SET_ENTRIES',
     entries: require('./src/entries.json')
 });
-store.dispatch({ type: 'NEXT' })
+
+store.dispatch({
+    type: 'NEXT'
+})
